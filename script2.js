7371b09841347be44731789229170fcc5da60fab
Greg <gdrew21@gmail.com>
2022-01-27 00:00:33 -0500

    barebones commit

--- script2.js
+++ script2.js
@@ -2,7 +2,7 @@
 
 const playerController = (() => {
     let playerClick = (square) => {
-        console.log(`${square}`)
+        console.log(`${square}`);
     }
 
     return { 
@@ -12,11 +12,21 @@ const playerController = (() => {
 })();
 
 const gameBoard = (() => {
-    let htmlBoard = Array.from(document.querySelector('#gbContainer').children)
-    console.log(htmlBoard)
+    let htmlBoard = Array.from(document.querySelector('#gbContainer').children);
+    let virtualBoard = new Array(9);
+
     let _init = (() => {
         htmlBoard.forEach((ele, idx) => {
-            ele.addEventListener('click', playerController.playerClick.bind(ele, idx))
+            ele.addEventListener('click', playerController.playerClick.bind(ele, idx));
         })
+
+        virtualBoard.fill({
+            clicked: null
+        });
     })();
+    console.log(virtualBoard);
+
+    _manageTurns = () => {
+        
+    }
 })();
